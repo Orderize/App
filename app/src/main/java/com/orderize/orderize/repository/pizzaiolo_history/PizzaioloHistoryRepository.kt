ec87c23ddb19be1c7b62a98d02f59eac0b636921
GabrielRodAmaral <gabriel.amaral@sptech.school>
2025-06-01 21:33:19 -0300

    Teste falha na build

--- app/src/main/java/com/orderize/orderize/repository/pizzaiolo_history/PizzaioloHistoryRepository.kt
+++ app/src/main/java/com/orderize/orderize/repository/pizzaiolo_history/PizzaioloHistoryRepository.kt
@@ -4,6 +4,6 @@ import com.orderize.orderize.repository.AppDataStore
 import com.orderize.orderize.repository.CommonRepository
 // comentário só para teste
 class PizzaioloHistoryRepository(
-    private val dataStore: AppDataStore
+    private val dataStore: AppDataSto
 ): CommonRepository(dataStore) {
 }
