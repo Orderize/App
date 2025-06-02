b4faced61fdcef85df53bfcf842499b30f0bb84a
GabrielRodAmaral <gabriel.amaral@sptech.school>
2025-06-01 22:39:57 -0300

    .

--- app/src/main/java/com/orderize/orderize/repository/pizzaiolo_history/PizzaioloHistoryRepository.kt
+++ app/src/main/java/com/orderize/orderize/repository/pizzaiolo_history/PizzaioloHistoryRepository.kt
@@ -2,7 +2,7 @@ package com.orderize.orderize.repository.pizzaiolo_history
 
 import com.orderize.orderize.repository.AppDataStore
 import com.orderize.orderize.repository.CommonRepository
-// comentário só para teste
+
 class PizzaioloHistoryRepository(
     private val dataStore: AppDataStore
 ): CommonRepository(dataStore) {
